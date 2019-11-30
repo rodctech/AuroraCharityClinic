246c604a7a0d2b990b3a4f139f1970a49a7e30e6
kdown1973 <38918047+kdown1973@users.noreply.github.com>
2019-11-30 15:19:02 -0600

    Add files via upload
    Add Item tab updated with labels, a clear button and two combo boxes. Database methods also updated, connection needs to be tested

---
+++ ACC_FXML_work/src/model/Main_ACC_WF.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main_ACC_WF extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		primaryStage.setTitle("ACC Database");
+		AnchorPane anchor = (AnchorPane) FXMLLoader.load(getClass().getResource("../view/Combine_GUI.fxml"));
+		Scene myScene = new Scene(anchor, 550, 450);  //180
+		primaryStage.setScene(myScene);
+		primaryStage.show();		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
